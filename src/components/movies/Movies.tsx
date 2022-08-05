c2dbf07b4703e3dfc88c6020debd90ae4ec1802a
Anna <perjesiannakatinka@gmail.com>
2022-08-05 10:53:03 +0200

    material ui datagrid init

--- src/components/movies/Movies.tsx
+++ src/components/movies/Movies.tsx
@@ -1,38 +1,16 @@
-import {
-  Box,
-  TableCell,
-  TableHead,
-  TableRow,
-  TableSortLabel,
-} from "@mui/material";
 import { inject, observer } from "mobx-react";
 import React from "react";
 import MainStore, { IMovie } from "../../core/stores/MainStore";
+import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
+import { toJS } from "mobx";
 
 interface IProps {
   MainStore?: MainStore;
 }
 
-interface HeadCell {
-  disablePadding: boolean;
-  id: keyof IMovie;
-  label: string;
-  numeric: boolean;
-}
-
-const headCells: readonly HeadCell[] = [
-  {
-    id: "id",
-    numeric: true,
-    disablePadding: false,
-    label: "Identifier",
-  },
-  {
-    id: "name",
-    numeric: false,
-    disablePadding: true,
-    label: "Title",
-  },
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "name", headerName: "Movie title", width: 500 },
 ];
 
 class Movies extends React.Component<IProps> {
@@ -40,39 +18,14 @@ class Movies extends React.Component<IProps> {
     const { MainStore } = this.props;
     return (
       <div>
-        <TableHead>
-          <TableRow>
-            {headCells.map((headCell) => (
-              <TableCell
-                key={headCell.id}
-                align={headCell.numeric ? "right" : "left"}
-                padding={headCell.disablePadding ? "none" : "normal"}
-                // sortDirection={orderBy === headCell.id ? order : false}
-              >
-                {/* <TableSortLabel
-              active={orderBy === headCell.id}
-              direction={orderBy === headCell.id ? order : 'asc'}
-              onClick={createSortHandler(headCell.id)}
-            >
-              {headCell.label}
-              {orderBy === headCell.id ? (
-                <Box component="span" sx={visuallyHidden}>
-                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                </Box>
-              ) : null}
-            </TableSortLabel> */}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-
-        {MainStore.movies?.map((movie: IMovie) => {
-          return (
-            <div key={movie.id}>
-              {movie.id} - {movie.name}
-            </div>
-          );
-        })}
+        <div style={{ height: 400, width: "100%" }}>
+          <DataGrid
+            rows={toJS(MainStore.movies)}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+          />
+        </div>
       </div>
     );
   }
